389ba1c70059ed09a4ae75604a14f7158f83880d
jacobkbny <pkg1028@gmail.com>
2023-03-31 10:20:42 +0900

    change Database prisma schema

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,40 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Headers,
+  HttpCode,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { CreateUserRequest } from './dtos/create_user_dto';
 import { DeleteWordDTO } from './dtos/delete_user_dto';
 import { WordResult } from './dtos/read_word_dto';
+import { RequestHeader } from './dtos/request_header_dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
- // Create
+  // Create
   @Post('/createUser')
-  createUser(@Body() createUserRequest: CreateUserRequest) {
-    return this.appService.insertUser(createUserRequest);
+  createUser(
+    @Headers() header: RequestHeader,
+    @Body() createUserRequest: CreateUserRequest,
+  ) {
+    const response = CheckHeader(header);
+    if (response) return this.appService.insertUser(createUserRequest);
+    else {
+      return HttpCode(400);
+    }
   }
 
   @Post('/registerword')
   registerWord(@Body() wordRequest: WordResult) {
     return this.appService.registerWord(wordRequest);
   }
- // Read
+  // Read
   @Get('/pendingword')
   getPendingWord() {
     return this.appService.getPendingWords();
@@ -32,15 +49,25 @@ export class AppController {
   getUserWord(): string {
     return '';
   }
- // Update
+  // Update
   @Put('/editword')
-  editWord(@Body() wordData: WordResult){
-    return this.appService.updateWordData(wordData)
+  editWord(@Body() wordData: WordResult) {
+    return this.appService.updateWordData(wordData);
   }
 
- // Delete
+  // Delete
   @Delete('/deleteword')
   deleteWord(@Body() deleteWordDTO: DeleteWordDTO) {
     return this.appService.deleteWordData(deleteWordDTO);
   }
+}
+
+function CheckHeader(header: RequestHeader) {
+  if (header.ApiKey !== process.env.Api_Key) {
+    return false;
+  }
+  if (header.secretKey !== process.env.Secret_Key) {
+    return false;
+  }
+  return true;
 }
---
+++ src/dtos/request_header_dto.ts
@@ -0,0 +1,4 @@
+export class RequestHeader{
+    ApiKey:string;
+    secretKey:string;
+}
